596939957197b9c329061760cc1b593e87bf9afa
J911 <jaemin.lee.email@gmail.com>
2019-07-24 15:07:36 +0900

    add: DenseNet

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # DenseNet-pytorch
-DenseNet: Densely Connected Convolutional Networks
+DenseNet: Densely Connected Convolutional Networks 
+
+A pytorch implementation of DenseNet([Huang, Gao, et al. “Densely connected convolutional networks.” Proceedings of the IEEE conference on computer vision and pattern recognition. 2017.](https://arxiv.org/abs/1608.06993)) for CIFAR10
+
+## Support & Requirements
+- 🔥pytorch >= 0.4.0
+- 🐍python 3.6.5 
+- 📈tensorboardX 1.8
+
+- ‼ multi GPU support!
+
+## Training
+git clone & change DIR
+```bash
+$ git clone https://github.com/J911/DenseNet-pytorch
+$ cd DenseNet-pytorch
+```
+training
+```bash
+$ python train.py
+```
+### optional arguments:    
+- --lr LR
+- --resume RESUME
+- --growth_rate GROWTH_RATE
+- --theta THETA
+- --dropout DROPOUT
+- --batch_size BATCH_SIZE
+- --batch_size_test BATCH_SIZE_TEST
+- --momentum MOMENTUM
+- --weight_decay WEIGHT_DECAY
+- --epoch EPOCH
+- --num_worker NUM_WORKER
+- --logdir LOGDIR
+
+## Results
+![result](/assets/RESULT.png)
+
+| color|growth_rate|theta|dropout|              scheduler|test error(%)|
+|:----:|:---------:|:---:|:-----:|:---------------------:|------------:|
+|Orange|         32|  0.5|    0.5|lr_decay [60, 120, 160]|         5.45| 
+|  Blue|         32|  0.5|    0.0|lr_decay [60, 120, 160]|         7.08|
+|  Pink|         32|  0.5|    0.0|      CosineAnnealingLR|         4.86|
---
+++ models/densenet.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import math
+
+class DenseLayer(nn.Module):
+    def __init__(self, n, growth_rate, droprate=0.0):
+        super(DenseLayer, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=growth_rate*n, out_channels=growth_rate, 
+                               kernel_size=1, stride=1)
+        self.conv2 = nn.Conv2d(in_channels=growth_rate, out_channels=growth_rate, 
+                               kernel_size=3, stride=1, padding=1)
+        
+        self.dropout = nn.Dropout(p=droprate)
+        self.bn1 = nn.BatchNorm2d(growth_rate)
+        self.bn2 = nn.BatchNorm2d(growth_rate)
+        self.relu = nn.ReLU(inplace=True)
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.dropout(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        
+        x = self.conv2(x)
+        x = self.dropout(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        
+        return x
+        
+class DenseBlock(nn.Module):
+    def __init__(self, growth_rate, num_layers, droprate=0.0):
+        super(DenseBlock, self).__init__()
+        self.layers = nn.ModuleList([DenseLayer(n+1, growth_rate, droprate) for n in range(num_layers)])
+        
+    def forward(self, x):        
+        for layer in self.layers:
+            x = torch.cat((x, layer(x)), 1)
+        return x
+    
+class TransitionBlock(nn.Module):
+    def __init__(self, growth_rate, theta, n, droprate=0.0):
+        super(TransitionBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels=growth_rate*(n+1), out_channels=growth_rate, 
+                               kernel_size=1, stride=1, padding=0)
+        self.avg_pool = nn.AvgPool2d(kernel_size=2, stride=math.ceil(1/theta))
+        
+        self.dropout = nn.Dropout(p=droprate)
+        self.bn = nn.BatchNorm2d(growth_rate)
+        self.relu = nn.ReLU(inplace=True)
+        
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.dropout(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        x = self.avg_pool(x)
+        
+        return x
+    
+class DenseNet(nn.Module):
+    def __init__(self, growth_rate=32, theta=0.5, num_layers=[12, 12, 12], droprate=0.0, num_classes=10):
+        super(DenseNet, self).__init__()       
+        self.conv = nn.Conv2d(in_channels=3, out_channels=growth_rate, 
+                               kernel_size=3, stride=1, padding=1)
+        self.dropout = nn.Dropout(p=droprate)
+        self.bn = nn.BatchNorm2d(growth_rate)
+        self.relu = nn.ReLU(inplace=True)
+        
+        self.block1 = DenseBlock(growth_rate, num_layers[0], droprate)
+        self.transitionBlock1 = TransitionBlock(growth_rate, theta, num_layers[0], droprate)
+        self.block2 = DenseBlock(growth_rate, num_layers[1], droprate)
+        self.transitionBlock2 = TransitionBlock(growth_rate, theta, num_layers[1], droprate)
+        self.block3 = DenseBlock(growth_rate, num_layers[2], droprate)
+
+        self.avg_pool = nn.AvgPool2d(kernel_size=8, stride=1)
+        self.fc = nn.Linear(growth_rate * (num_layers[-1] + 1), num_classes)
+                                     
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.dropout(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        
+        x = self.block1(x)
+        x = self.transitionBlock1(x)
+        
+        x = self.block2(x)
+        x = self.transitionBlock2(x)
+        
+        x = self.block3(x)
+
+        x = self.avg_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        
+        return x
